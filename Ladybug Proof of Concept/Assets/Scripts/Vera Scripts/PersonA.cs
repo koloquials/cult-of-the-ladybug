0ea466f85383d205dd723148ede3460f1b17b23f
Vera Limani <vl979@nyu.edu>
2018-10-26 16:32:46 -0400

    Added Person A and Person B classes

---
+++ Ladybug Proof of Concept/Assets/Scripts/Vera Scripts/PersonA.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonA : NPC {
+
+    public DialogueTree mainTree1, mainTree2, mainTree3;
+    public DialogueTree loopTree1, loopTree2, loopTree3;
+
+    public override void Start () {
+        base.Start();
+	}
+
+    public override void Update()
+    {
+        base.Update();
+
+        //load tree 1 if info 1 is true
+        if (!variables.infoOne)
+        {
+
+            treeToLoad = mainTree1;
+
+        }
+        else if (variables.infoOne)
+        {
+
+            treeToLoad = loopTree1;
+
+            //load tree 2 if info three is false and angry at player is true
+        }
+        else if (variables.infoThree != true && AngryAtPlayer)
+        {
+
+            treeToLoad = mainTree2;
+
+        }
+        else if (variables.infoTwo)
+        {
+
+            treeToLoad = loopTree2;
+
+        }
+        else if (variables.infoThree)
+        {
+
+            treeToLoad = mainTree3;
+        }
+        else if (variables.infoFive)
+        {
+            treeToLoad = loopTree3;
+        }
+
+    }
+}
---
+++ Ladybug Proof of Concept/Assets/Scripts/Vera Scripts/PersonB.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonB : NPC {
+
+
+    public DialogueTree mainTree1, mainTree2, mainTree3;
+    public DialogueTree loopTree1, loopTree2, loopTree3;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+    public override void Update()
+    {
+        base.Update();
+
+        if (!AngryAtPlayer)
+        {
+
+            treeToLoad = mainTree1;
+
+        }
+        else if (AngryAtPlayer)
+        {
+
+            treeToLoad = loopTree1;
+
+        }
+        else if ((variables.infoOne && variables.infoFour == false && !AngryAtPlayer) || (variables.infoTwo && !variables.infoFour))
+        {
+
+            treeToLoad = mainTree2;
+
+        } else if((variables.infoOne && variables.infoFour == false && AngryAtPlayer) || (variables.infoTwo && !variables.infoFour)){
+
+            treeToLoad = loopTree2;
+
+        
+        } else if(variables.infoFour){
+
+            treeToLoad = mainTree3;
+
+        } else if(variables.infoSix){
+
+            treeToLoad = loopTree3;
+
+        }
+    }
+}
