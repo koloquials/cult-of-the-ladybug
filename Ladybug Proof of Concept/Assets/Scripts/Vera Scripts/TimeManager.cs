4bd69e26a2dbefd0840930e8064490e7870db554
Vera Limani <vl979@nyu.edu>
2018-10-28 10:05:47 -0400

    Added Game Over stuff

--- Ladybug Proof of Concept/Assets/Scripts/Vera Scripts/TimeManager.cs
+++ Ladybug Proof of Concept/Assets/Scripts/Vera Scripts/TimeManager.cs
@@ -2,26 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimeManager : MonoBehaviour {
 
     public float timeLimit, currentTime, modifier;
+    DialogueManager manager;
+
+    public GameObject gameOverText;
+    public int GameOverSceneIndex;
 
     private void Start()
     {
         currentTime = timeLimit;
+        manager = FindObjectOfType<DialogueManager>();
+        gameOverText.SetActive(false);
     }
 
     private void Update()
     {
         DisplayTime();
-        currentTime -= (Time.deltaTime * modifier);
+
+        if(currentTime<=0f){
+            
+            manager.currentGameState = DialogueManager.GameState.TimerOut;
+        }
+
+        if (manager.currentGameState != DialogueManager.GameState.TimerOut)
+        {
+            currentTime -= (Time.deltaTime * modifier);
+        }
+
+        if(manager.currentGameState == DialogueManager.GameState.TimerOut){
+
+            gameOverText.SetActive(true);
+        }
+
         
     }
 
     void DisplayTime(){
         GameObject timertext = GameObject.Find("TimerText");
         Text tt = timertext.GetComponent<Text>();
-        tt.text = "" + Mathf.FloorToInt(currentTime);
+        tt.text = "" + Mathf.CeilToInt(currentTime);
     }
 }
