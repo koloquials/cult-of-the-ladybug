66d0664f725b730423874091284bd96a3cf23767
Unknown <rg3112@nyu.edu>
2018-11-14 15:25:52 -0500

    Finalized "Duel" changes.

--- Ladybug Proof of Concept/Assets/Scripts/Duel Assets/ExampleEnemy.cs
+++ Ladybug Proof of Concept/Assets/Scripts/Duel Assets/ExampleEnemy.cs
@@ -7,33 +7,34 @@ public class ExampleEnemy : Enemy{
     void Start () {
         //items to replace will be {inside these brackets!}, write inside them/follow their instructions and then delete the brackets!
         name = "[{NAME OF ENEMY}]\n";
+        startPos = 3; //1-6. whatever tile they start on. 3 by default, which means they are on the fourth tile.
 
         dialogue.Add("\n“{intro text}”");
+        returnText = "{text if the player rematches npc}";
 
-        dialogue.Add("[DUEL:START]\n");
-        options.Add("{concede option}\n");
-        options.Add("{stand ground option}\n");
-        options.Add("{aggressive option}\n");
-        options.Add("{insult option}\n");
-        dialogue.Add("\"{enemy's response}\"\n");
-        types.Add(0); // <- replace with 1(concede)/2(stand)/3(aggress)/4(insult) depending on the tone of enemy response. 
-        //it's important that you stick to ~2 types and properly ramp up to them or else the player will think they're unpredictable.
-
-        dialogue.Add("\"{copy and paste the enemy's response from above, that's it.}\"\n");
-        options.Add("{concede option}\n");
-        options.Add("{stand ground option}\n");
-        options.Add("{aggressive option}\n");
-        options.Add("{insult option}\n");
-        dialogue.Add("\"{enemy's response}\"\n");
-        types.Add(0); // <- replace with 1(concede)/2(stand)/3(aggress)/4(insult) depending on the tone of enemy response. 
+        dialogue.Add("[INTERROGATION:START]\n");
+        options.Add("{very bad option}\n"); //1
+        options.Add("{bad option}\n"); //2
+        options.Add("{okay option}\n"); //3
+        options.Add("{great option}\n"); //4
+        dialogue.Add("\"{enemy's response}\"\n"); //as in the next prompt, not rly a positive or negative response to what the player said. 
+        types.Add(4);
 
         dialogue.Add("\"{copy and paste the enemy's response from above, that's it.}\"\n");
-        options.Add("{concede option}\n");
-        options.Add("{stand ground option}\n");
-        options.Add("{aggressive option}\n");
-        options.Add("{insult option}\n");
+        options.Add("{very bad option}\n"); //1
+        options.Add("{bad option}\n"); //2
+        options.Add("{okay option}\n"); //3
+        options.Add("{great option}\n"); //4
         dialogue.Add("\"{enemy's response}\"\n");
-        types.Add(0); // <- replace with 1(concede)/2(stand)/3(aggress)/4(insult) depending on the tone of enemy response.
+        types.Add(4);
+
+        dialogue.Add("\"{copy and paste the enemy's response from above, that's it.}\"\n");
+        options.Add("{very bad option}\n"); //1
+        options.Add("{bad option}\n"); //2
+        options.Add("{okay option}\n"); //3
+        options.Add("{great option}\n"); //4
+        dialogue.Add("\"{enemy's response}\"\n");
+        types.Add(4);
 
         //rinse and repeat!!
     }
