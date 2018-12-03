3a5c6037623b2f7459e05eb5907292991b12b0a6
Unknown <rg3112@nyu.edu>
2018-12-03 12:50:27 -0500

    Multiple Fight Integration

--- Ladybug Proof of Concept/Assets/Scripts/Vera Scripts/ConditionalDuelManager.cs
+++ Ladybug Proof of Concept/Assets/Scripts/Vera Scripts/ConditionalDuelManager.cs
@@ -10,6 +10,7 @@ public class ConditionalDuelManager : MonoBehaviour {
     [System.Serializable]
     public struct DuelCondition {
         public List<Clue> cluesNeededForCondition;
+        public DialogueTree newInfoReward;
         public int duelId;
     }
 
@@ -47,6 +48,7 @@ public class ConditionalDuelManager : MonoBehaviour {
 
         if(conditionsMet == condition.cluesNeededForCondition.Count){
             currentDuelCondition = condition;
+            nPC.informationReward = condition.newInfoReward;
         }
     }
 }
