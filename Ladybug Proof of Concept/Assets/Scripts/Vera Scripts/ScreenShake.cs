5c43ad0a1322443a576ef7ee055afb5a7eb481e1
Vera Limani <vl979@nyu.edu>
2018-12-09 09:43:50 -0500

    Tweaking camera movements, npc placement, cutscene revamps

---
+++ Ladybug Proof of Concept/Assets/Scripts/Vera Scripts/ScreenShake.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour {
+
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+
+    Vector3 originalPos;
+
+    void Update()
+    {
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    public void Shake(){
+        //StartCoroutine("Screen");
+    }
+
+    public IEnumerator Screen()
+    {
+        //camTransform.localPosition = originalPos + Random.insideUnitSphere * Mathf.Sin(shakeAmount);
+
+        originalPos = camTransform.localPosition;
+
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+            yield return null;
+        }
+
+    }
+
+
+}
