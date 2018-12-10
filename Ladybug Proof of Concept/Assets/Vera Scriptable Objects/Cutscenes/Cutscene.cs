986a4f342804d3d170665d5c3cb3fa95c2ada1f2
LEOPOLD\allis <allisongameyfuntimes@gmail.com>
2018-12-10 14:16:50 -0500

    fixed the merge conflict

--- Ladybug Proof of Concept/Assets/Vera Scriptable Objects/Cutscenes/Cutscene.cs
+++ Ladybug Proof of Concept/Assets/Vera Scriptable Objects/Cutscenes/Cutscene.cs
@@ -1,33 +1,3 @@
-<<<<<<< HEAD:Ladybug Proof of Concept/Assets/Scriptable Objects/Cutscenes/Cutscene.cs
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-[CreateAssetMenu(fileName = "NewCutscene", menuName = "Cutscene")]
-public class Cutscene : ScriptableObject {
-
-    [System.Serializable]
-    public struct Scene {
-        public Actor[] actorsInScene;
-        public Actor currentSpeaker;
-        [TextArea(4,6)]
-        public string[] linesInScene;
-        public float blackfadeAlpha;
-        public bool narration;
-    }
-
-    [System.Serializable]
-    public struct Actor {
-        public NpcTemplate thisActor;
-        public DialogueNode.Tone tone;
-    }
-
-    public Scene[] scenesInCutscene;
-
-	
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -57,4 +27,3 @@ public class Cutscene : ScriptableObject {
 
 	
 }
->>>>>>> a8e6a60b39f5e463be6a0ce481abf36c6ea12ed9:Ladybug Proof of Concept/Assets/Vera Scriptable Objects/Cutscenes/Cutscene.cs
